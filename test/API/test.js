9c8a37459834567232653f4edaf1bc3e13f8f965
Jordan Doig <jordandoig@live.com>
2018-02-11 09:27:24 -0700

    Added tests to verify response body

--- test/API/test.js
+++ test/API/test.js
@@ -9,10 +9,27 @@ it("Should return a response from the API.", () => {
   });
 });
 
-it("Should return a response from the API with status code 200.", () => {
+it("Should return a response from the API with status 200.", () => {
   var req = request('https://shapeshift.io').get('/rate/btc_ltc');
   req.end((err, res) => {
-    var statusCode = res.res.statusCode;
-    assert.equal(200, statusCode, 'ShapeShift API should return a response with status code 200 from GET /rate/btc_ltc.');
+    var status = res.res.status;
+    assert.equal(200, status, 'ShapeShift API should return a response with status 200 from GET /rate/btc_ltc.');
+  });
+});
+
+it("Should return a response from the API with a pair that matches the request pair.", () => {
+  var reqPair = 'btc_ltc';
+  var req = request('https://shapeshift.io').get('/rate/' + reqPair);
+  req.end((err, res) => {
+    var resPair = res.res.body.pair;
+    assert.equal(reqPair, resPair, 'ShapeShift API should return a response where the response pair matches the request pair.');
+  });
+});
+
+it("Should return a response from the API with a rate.", () => {
+  var req = request('https://shapeshift.io').get('/rate/btc_ltc');
+  req.end((err, res) => {
+    var rate = res.res.body.rate;
+    assert.exists(rate, 'ShapeShift API should return a response with a rate.');
   });
 });
