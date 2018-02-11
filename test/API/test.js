e46d14587998157fcd5cc757817c2e188c74ea8d
Jordan Doig <jordandoig@live.com>
2018-02-11 08:58:28 -0700

    Incorporated chai-http into chai setup, added first two API tests

--- test/API/test.js
+++ test/API/test.js
@@ -1,6 +1,18 @@
 var common = require("../common");
 var assert = common.assert;
+var request = common.request;
 
-it("", () => {
+it("Should return a response from the API.", () => {
+  var req = request('https://shapeshift.io').get('/rate/btc_ltc');
+  req.end((err, res) => {
+    assert.exists(statusCode, 'ShapeShift API should return a response from GET /rate/btc_ltc.');
+  });
+});
 
+it("Should return a response from the API with status code 200.", () => {
+  var req = request('https://shapeshift.io').get('/rate/btc_ltc');
+  req.end((err, res) => {
+    var statusCode = res.res.statusCode;
+    assert.equal(200, statusCode, 'ShapeShift API should return a response with status code 200 from GET /rate/btc_ltc.');
+  });
 });
--- test/common.js
+++ test/common.js
@@ -1,7 +1,10 @@
 var chai = require("chai");
+var chaiHttp = require("chai-http");
+chai.use(chaiHttp);
 
 var options = {};
 
 exports.options = options;
 exports.chai = chai;
 exports.assert = chai.assert;
+exports.request = chai.request;
