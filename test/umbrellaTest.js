294495c97ea04007f719b1d2356158c635d6cf58
Jordan Doig <jordandoig@live.com>
2018-02-11 14:16:46 -0700

    Removed clutter from umbrella and common files

--- test/umbrellaTest.js
+++ test/umbrellaTest.js
@@ -5,13 +5,7 @@ function importTest(name, path) {
 }
 
 describe("Testing:", function () {
-    // beforeEach(function () {
-    //    console.log("Testing...");
-    // });
     importTest("MyMath", './MyMath/test');
     importTest("API - GET /rate/[pair]", './API/rate');
     importTest("API - GET /getcoins", './API/getcoins');
-    // after(function () {
-    //     console.log("Tests Complete");
-    // });
 });
