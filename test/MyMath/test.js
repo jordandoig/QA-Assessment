d4070141d35ec384221d3668bf6b0ed66cf6944b
Jordan Doig <jordandoig@live.com>
2018-02-10 17:02:46 -0700

    Added simple tests for the MyMath methods

--- test/MyMath/test.js
+++ test/MyMath/test.js
@@ -14,3 +14,24 @@ it("Should return 4 when 3 and 1 are added.", () => {
 
   assert.strictEqual(4,result);
 });
+
+it("Should return 3 when 6 is subtracted from 9.", () => {
+  var instance = new MyMath();
+  var result = instance.subtract(9,6);
+
+  assert.strictEqual(3,result);
+});
+
+it("Should return 21 when 7 and 3 are multiplied.", () => {
+  var instance = new MyMath();
+  var result = instance.multiply(7,3);
+
+  assert.strictEqual(21,result);
+});
+
+it("Should return 5 when 30 is divided by 6.", () => {
+  var instance = new MyMath();
+  var result = instance.divide(30,6);
+
+  assert.strictEqual(5,result);
+});
