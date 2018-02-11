6b691019dd285f7e2465d9346ff7e8e68b62b86d
Jordan Doig <jordandoig@live.com>
2018-02-10 20:31:22 -0700

    Added directory for API tests and registered test file in top level test file

---
+++ test/API/test.js
@@ -0,0 +1,6 @@
+var common = require("../common");
+var assert = common.assert;
+
+it("", () => {
+
+});
--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ describe("test", function () {
     //    console.log("Testing...");
     // });
     importTest("MyMath", './MyMath/test');
+    importTest("API", './API/test');
     // after(function () {
     //     console.log("Tests Complete");
     // });
